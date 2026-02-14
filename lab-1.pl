bb095994c975ac906e174790f8a964ca843b97b2
Ivan <aleshin.ik@phystech.edu>
2026-02-14 10:57:15 +0300

    feat: add 4.2 withot format

--- lab-1.pl
+++ lab-1.pl
@@ -19,7 +19,7 @@ delta_aux(Prev, [H|T], [Diff|Rest]) :-
     Diff is H - Prev,
     delta_aux(H, T, Rest).
 
-% 4.
+% 4.1 Автоматическая
 int_to_binary(0, "0") :- !.
 int_to_binary(N, S) :-
     N > 0, !,
@@ -31,6 +31,30 @@ int_to_binary(N, S) :-
     format(atom(A), '~2r', [N1]),
     atom_string(A, S1),
     string_concat("-", S1, S).
+
+% 4.2 Ручная
+% 4.
+int_to_binary_2(0, "0") :- !.
+int_to_binary_2(N, S) :-
+    N > 0, !,
+    collect_bits(N, [], Bits),
+    bits_to_string(Bits, S).
+int_to_binary_2(N, S) :-
+    N < 0,
+    N1 is -N,
+    collect_bits(N1, [], Bits),
+    bits_to_string(Bits, S1),
+    string_concat("-", S1, S).
+
+collect_bits(0, Acc, Acc) :- !.
+collect_bits(N, Acc, Bits) :-
+    Bit is N mod 2,
+    N1 is N // 2,
+    collect_bits(N1, [Bit|Acc], Bits).
+
+bits_to_string(Bits, S) :-
+    maplist([B, C]>>(C is B + 0'0), Bits, Codes),
+    string_codes(S, Codes).
 
 % 5.
 rle_encode([], []).
@@ -59,6 +83,8 @@ run_tests :-
     delta([1,2,4,3], D),
     format("3. ~w~n", [D]),
     int_to_binary(8, B1), int_to_binary(-2, B2), int_to_binary(0, B0),
+    format("4. ~w ~w ~w~n", [B1, B2, B0]),
+    int_to_binary_2(8, B1), int_to_binary(-2, B2), int_to_binary(0, B0),
     format("4. ~w ~w ~w~n", [B1, B2, B0]),
     rle_encode([a,a,a,b,c,c,a,a], E),
     format("5. ~w~n", [E]).
