08a475b40c81975fc3a1d4d5790192967877e52c
ivan-aleschin <aleshin.ik@phystech.edu>
2026-03-28 09:10:48 +0300

    fix: ex3

--- logic-programming/lab-4/parent_children.erl
+++ logic-programming/lab-4/parent_children.erl
@@ -1,5 +1,4 @@
 -module(parent_children).
-
 -export([start/1, send_to_child/2, stop/0]).
 
 %% Public API
@@ -7,7 +6,7 @@
 start(N) when is_integer(N), N >= 0 ->
     case whereis(parent_children) of
         undefined ->
-            ParentPid = spawn(fun() -> parent_loop(#{}, N) end),
+            ParentPid = spawn(fun() -> parent_init(N) end),
             register(parent_children, ParentPid),
             ok;
         _Pid ->
@@ -32,59 +31,51 @@ stop() ->
 
 %% Parent process
 
-parent_loop(Children, N) ->
+parent_init(N) ->
     process_flag(trap_exit, true),
-    Children1 =
-        case maps:size(Children) of
-            0 -> spawn_children(1, N, #{});
-            _ -> Children
-        end,
+    Children = spawn_children(1, N, #{}),
+    parent_loop(Children).
+
+parent_loop(Children) ->
     receive
         {send_to_child, I, Msg} ->
-            case maps:get(I, Children1, undefined) of
+            case maps:get(I, Children, undefined) of
                 undefined ->
-                    io:format("Parent: no child ~p~n", [I]),
-                    ok;
+                    io:format("Parent: no child ~p~n", [I]);
                 Pid ->
                     Pid ! Msg
             end,
-            parent_loop(Children1, N);
-
+            parent_loop(Children);
         stop ->
             unregister(parent_children),
             lists:foreach(fun({_I, Pid}) -> Pid ! stop end,
-                          maps:to_list(Children1)),
+                          maps:to_list(Children)),
             ok;
-
         {'EXIT', Pid, Reason} ->
-            case child_index_by_pid(Pid, Children1) of
+            case child_index_by_pid(Pid, Children) of
                 undefined ->
-                    parent_loop(Children1, N);
+                    parent_loop(Children);
                 I ->
                     case Reason of
                         normal ->
-                            Children2 = maps:remove(I, Children1),
-                            parent_loop(Children2, N);
+                            parent_loop(maps:remove(I, Children));
                         shutdown ->
-                            Children2 = maps:remove(I, Children1),
-                            parent_loop(Children2, N);
+                            parent_loop(maps:remove(I, Children));
                         _ ->
                             io:format("Parent: child ~p died with ~p, restarting~n", [I, Reason]),
-                            NewPid = spawn_child(I, self()),
-                            Children2 = Children1#{I => NewPid},
-                            parent_loop(Children2, N)
+                            NewPid = spawn_child(I),
+                            parent_loop(Children#{I => NewPid})
                     end
             end
     end.
 
-spawn_children(I, N, Acc) when I > N ->
-    Acc;
+spawn_children(I, N, Acc) when I > N -> Acc;
 spawn_children(I, N, Acc) ->
-    Pid = spawn_child(I, self()),
+    Pid = spawn_child(I),
     spawn_children(I + 1, N, Acc#{I => Pid}).
 
-spawn_child(I, ParentPid) ->
-    spawn_link(fun() -> child_loop(I, ParentPid) end).
+spawn_child(I) ->
+    spawn_link(fun() -> child_loop(I) end).
 
 child_index_by_pid(Pid, Children) ->
     case lists:keyfind(Pid, 2, maps:to_list(Children)) of
@@ -94,13 +85,11 @@ child_index_by_pid(Pid, Children) ->
 
 %% Child process
 
-child_loop(I, _ParentPid) ->
+child_loop(I) ->
     receive
-        stop ->
-            ok;
-        die ->
-            exit({child_died, I});
-        Msg ->
+        stop -> ok;
+        die  -> exit({child_died, I});
+        Msg  ->
             io:format("Child ~p (~p) got message: ~p~n", [I, self(), Msg]),
-            child_loop(I, _ParentPid)
+            child_loop(I)
     end.
