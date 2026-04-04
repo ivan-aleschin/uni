62e0679181995766e1b1e4c624dae398fa4445bb
ivan-aleschin <aleshin.ik@phystech.edu>
2026-04-04 14:48:44 +0300

    feat: delete pessenger for lab-3

--- software-architecture/lab-3/src/main.cpp
+++ software-architecture/lab-3/src/main.cpp
@@ -2,18 +2,18 @@
 #include <iostream>
 
 int main() {
-    Airplane plane(3500); 
+    Airplane plane(3500);
 
     plane.assembleCrewAndPassengers();
 
     plane.printLoadingMap();
 
     std::cout << "\n--- Демонстрация удаления пассажира ---\n";
-    std::string victim = "Alice_Brown";
+    std::string victim = "Bob_Wilson";
     if (plane.removePassenger(victim)) {
-        std::cout << "Пассажир удалён: " << victim << "\n";
+        std::cout << "Пассажир удалён: " << victim << "\n\n";
     } else {
-        std::cout << "Пассажир не найден: " << victim << "\n";
+        std::cout << "Пассажир не найден: " << victim << "\n\n";
     }
 
     plane.printLoadingMap();
